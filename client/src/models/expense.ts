403b0d9a65b15369555b9d3f177002a9ebe3bb09
Somendu <somendu.maiti@gmail.com>
2023-11-28 15:39:13 +0530

    First Commit
    Signed-off-by: Somendu <somendu.maiti@gmail.com>

---
+++ client/src/models/expense.ts
@@ -0,0 +1,9 @@
+interface IExpenseItem {
+    expenseDescription: string,
+      payeeName : string,
+      price: number ,
+      date: Date,
+      expenseDate: string,
+      id: number, 
+}
+export default IExpenseItem;
---
+++ client/src/services/expense-service.ts
@@ -0,0 +1,28 @@
+
+
+import axios from "axios";
+import IExpenseItem from '../models/expense';
+
+const getAllExpenseItems = async () => {
+
+  const response = await axios.get("http://localhost:3000/expenses")
+
+  return response.data;
+}
+
+const createNewExpenseItem = async (expense: IExpenseItem) => {
+  const response = await axios.post("http://localhost:3000/expenses", expense);
+  return response.data;
+}
+
+const updateExpenseItem = async (updatedExpense: IExpenseItem) => {
+  const response = await axios.put(`http://localhost:3000/expenses/${updatedExpense.id}`, updatedExpense);
+  return response.data;
+}
+
+const deleteExpenseItem = async (expenseId: number) => {
+  const response = await axios.delete(`http://localhost:3000/expenses/${expenseId}`);
+  return response.data;
+}
+
+export {getAllExpenseItems, createNewExpenseItem, updateExpenseItem, deleteExpenseItem};
